72e14c674c6aa4e7a893739a69065759e25c3500
Vivek Gnanasekaran <vishavishal@gmail.com>
2022-07-02 09:54:15 +0530

    User authentication added

---
+++ test/models/user_test.rb
@@ -0,0 +1,51 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                    password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "Name should be present" do
+    @user.name = " "
+    assert_not @user.valid?
+  end
+
+  test "Email should be presence" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "Email should be valid" do
+    @user.email = "vvv@gmail@com"
+    assert_not @user.valid?
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+end

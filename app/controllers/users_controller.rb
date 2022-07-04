153e575c35b46a57027c63f0097151d6d42ed3c6
Vivek Gnanasekaran <vishavishal@gmail.com>
2022-07-04 16:44:12 +0530

    User form added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+  before_action :get_user
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = get_user 
+  end
+
+  def create 
+    @user = User.new(params[:user]) # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private 
+  def get_user 
+    @user = User.find_by(id: params[:id])
+    @user
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email) 
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    #https://source.unsplash.com/random/200x200?sig=1
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
